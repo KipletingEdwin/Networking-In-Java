aa43cf54ac9c500a03781fa9b060d342faf0ff5d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-12 15:54:34 +0300

    add getAddress method and getHostAddress method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws UnknownHostException {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String url = "www.simplilearn.com";
+
+        InetAddress inetAddress = InetAddress.getByName(url);
+
+        System.out.println("Address : " + Arrays.toString(inetAddress.getAddress()));
+        //getHost Address method
+        System.out.println("Host Address : " + inetAddress.getHostAddress());
+
     }
 }
